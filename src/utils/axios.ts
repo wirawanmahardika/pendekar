b785012ccd1bc07b11b9e80c634721128c43a9e3
wirawanmahardika <wiraprogrammer@gmail.com>
2025-04-10 09:31:39 +0800

    chore: auto redirect ke login page jika status code response dari server adalah 401

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -5,19 +5,10 @@ export const AxiosAuth = axios.create({
   headers: { Authorization:  getApiToken() }
 })
 
-axios.interceptors.request.use((config) => {
-  const token = getApiToken();
-  config.headers.Authorization = token;
-  return config;
-});
-
-axios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error?.response?.status === 401) {
-      clearApiToken();
-      window.location.href = "/";
-    }
-    return Promise.reject(error);
+AxiosAuth.interceptors.response.use((response) => response, (error) => {
+  if(error?.response.status === 401){
+    clearApiToken()
+    window.location.href = '/login'
   }
-);
+  return Promise.reject(error)
+})
