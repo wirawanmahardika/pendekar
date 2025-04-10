a3d9b2cce4ae198bb68226559d0874549f215793
Muhammad Fauzan Amir <57846733+fauzanmr12@users.noreply.github.com>
2025-04-10 14:51:25 +0800

    add: membuat komponen tabel dan card administrasi

--- src/components/administrasi/AdministrasiViewFilterAndCard.tsx
+++ src/components/administrasi/AdministrasiViewFilterAndCard.tsx
@@ -1,14 +1,16 @@
 import { useMemo, useState } from "react";
-import { FaMapLocation } from "react-icons/fa6";
 import { getSlugType, KODE_SLUG } from "../../utils/api";
+import AdministrasiCard from "./AdministrasiCard";
 
 const AdministrasiDataCard = (props: any) => {
-  const { listKecamatan, listDesa } = props.administraionData.data;
+  console.log(props);
+  const { list_kecamatan, list_desa } = props.administrationData.data;
+
   const [selectedDesa, setSelectedDesa] = useState("");
 
   const [selectedKec, setSelectedKec] = useState(
     getSlugType() === "kecamatan"
-      ? listKecamatan.find(
+      ? list_kecamatan.find(
           (kec: any) =>
             kec.nama_kecamatan.toLowerCase() ===
             KODE_SLUG.split("-")[0].toLowerCase()
@@ -16,26 +18,26 @@ const AdministrasiDataCard = (props: any) => {
       : ""
   );
 
-  const FilteredDesa = useMemo(() => {
+  const listDesa = useMemo(() => {
     setSelectedDesa("");
     if (selectedKec) {
-      return listDesa.filter((desa: any) =>
+      return list_desa.filter((desa: any) =>
         desa.kode_wilayah.startsWith(selectedKec)
       );
     }
     return [];
-  }, [listDesa, selectedKec]);
+  }, [list_desa, selectedKec]);
 
   const filteredKec = useMemo(() => {
     if (getSlugType() === "kecamatan") {
       const kecamatanName = KODE_SLUG.split("-")[0];
-      return listKecamatan.filter(
+      return list_kecamatan.filter(
         (kec: any) =>
           kec.nama_kecamatan.toLowerCase() === kecamatanName.toLowerCase()
       );
     }
-    return listKecamatan;
-  }, [listKecamatan]);
+    return list_kecamatan;
+  }, [list_kecamatan]);
 
   const dataAdministrasi = useMemo(() => {
     let buku_peraturan_di_desa = 0;
@@ -140,7 +142,7 @@ const AdministrasiDataCard = (props: any) => {
           );
         });
       } else {
-        listDesa.forEach((desa: any) => {
+        list_desa.forEach((desa: any) => {
           buku_peraturan_di_desa += parseInt(desa.buku_peraturan_di_desa);
           buku_peraturan_di_desa_add += parseInt(
             desa.buku_peraturan_di_desa_add
@@ -201,7 +203,7 @@ const AdministrasiDataCard = (props: any) => {
       buku_lembaran_desa_dan_berita_desa,
       buku_lembaran_desa_dan_berita_desa_add,
     };
-  }, [listDesa, listDesa, selectedDesa, selectedKec]);
+  }, [listDesa, list_desa, selectedDesa, selectedKec]);
 
   return (
     <div className="bg-white shadow rounded-lg p-6">
@@ -227,7 +229,7 @@ const AdministrasiDataCard = (props: any) => {
           className="focus:border-blue-400 focus:shadow border text-sm border-slate-300 rounded text-neutral-600 w-1/4 outline-none pl-2 pr-4 py-1"
         >
           <option value="">Semua Desa</option>
-          {listDesa.map((item: any) => {
+          {list_desa.map((item: any) => {
             return (
               <option
                 key={item.kode_wilayah}
@@ -240,21 +242,58 @@ const AdministrasiDataCard = (props: any) => {
           })}
         </select>
       </div>
-
-      {/* //card */}
       <div className="pt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="flex justify-between rounded-lg shadow-md p-4">
-          <div className="flex gap-x-4 items-center">
-            <div className="bg-cyan-100 rounded-full p-3">
-              <FaMapLocation size="30" className="text-cyan-600" />
-            </div>
-            <div className="flex-col">
-              <h3 className="text-sm ">Peraturan Desa</h3>
-              <p className="text-black text-2xl font-semibold">10</p>
-            </div>
-          </div>
-          <div className="text-sm text-emerald-400">+100/Minggu</div>
-        </div>
+        <AdministrasiCard
+          title={"Peraturan Desa"}
+          data={dataAdministrasi.buku_peraturan_di_desa}
+          dataPerWeek={dataAdministrasi.buku_peraturan_di_desa_add}
+        />
+
+        <AdministrasiCard
+          title={"Keputusan Kepala Desa"}
+          data={dataAdministrasi.buku_keputusan_kepala_desa}
+          dataPerWeek={dataAdministrasi.buku_keputusan_kepala_desa_add}
+        />
+
+        <AdministrasiCard
+          title={"Inventaris Desa"}
+          data={dataAdministrasi.buku_inventaris_dan_kekayaan_desa}
+          dataPerWeek={dataAdministrasi.buku_inventaris_dan_kekayaan_desa_add}
+        />
+
+        <AdministrasiCard
+          title={"Aparat Pemerintah Desa"}
+          data={dataAdministrasi.buku_aparat_pemerintah_desa}
+          dataPerWeek={dataAdministrasi.buku_aparat_pemerintah_desa_add}
+        />
+
+        <AdministrasiCard
+          title={"Buku Agenda"}
+          data={dataAdministrasi.buku_agenda}
+          dataPerWeek={dataAdministrasi.buku_agenda_add}
+        />
+
+        <AdministrasiCard
+          title={"Lembaran & Berita Desa"}
+          data={dataAdministrasi.buku_lembaran_desa_dan_berita_desa}
+          dataPerWeek={dataAdministrasi.buku_lembaran_desa_dan_berita_desa_add}
+        />
+
+        <AdministrasiCard
+          title={"Tanah Kas Desa"}
+          data={dataAdministrasi.buku_tanah_kas_desa}
+          dataPerWeek={dataAdministrasi.buku_tanah_kas_desa_add}
+        />
+
+        <AdministrasiCard
+          title={"Luas Tanah Desa"}
+          data={
+            <>
+              {dataAdministrasi.buku_tanah_di_desa}m<sup>2</sup>
+            </>
+          }
+          dataPerWeek={dataAdministrasi.buku_tanah_di_desa}
+        />
       </div>
     </div>
   );
