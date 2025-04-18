7132d34b7e7c7b12a8ff7e1b9917752c40bc512c
wirawanmahardika <wiraprogrammer@gmail.com>
2025-04-18 16:15:46 +0800

    chore: mempercepat menampilkan data ketika filter berubah pada page wisata

--- src/pages/Wisata.tsx
+++ src/pages/Wisata.tsx
@@ -16,45 +16,28 @@ export default function Wisata() {
 
     const [loading, setIsLoading] = useState(false)
     const resultData = useGetResultData<wisataDataType>(`${BASE_API_URL}wisata?k3=&k4=&search=&limit=100`, setIsLoading)
-    
+    if (loading) return <LoadingDots />
+
     const [search, setSearch] = useState({ text: "", kecamatan: "", desa: "" })
     const [dataTodisplay, setDataToDisplay] = useState<wisataCardType[]>()
-    
+
     useEffect(() => {
-        const idTimeout = setTimeout(() => {
-            AxiosAuth
-            .get(`${BASE_API_URL}wisata?k3=${search.kecamatan}&k4=${search.desa}&search=${search.text}&limit=100`)
-            .then((result) => {
-                setDataToDisplay(result.data.data.list_wisata)
-            })
+        AxiosAuth
+            .get(`${BASE_API_URL}wisata?k3=&k4=&search=&limit=100`)
+            .then((result) => setDataToDisplay(result.data.data.list_wisata))
             .catch((error) => alert(error.message))
-        }, 500);
-        return () => clearTimeout(idTimeout)
+    }, [])
+
+    useEffect(() => {
+        const listWisata = resultData?.list_wisata.filter(lb => {
+            let status: boolean = true;
+            if (search.kecamatan) status = status && lb.k3 === search.kecamatan
+            if (search.desa) status = status && lb.k4 === search.desa
+            if (search.text) status = status && lb.nama_deskel.toLowerCase().includes(search.text.toLowerCase())
+            return status
+        })
+        setDataToDisplay(listWisata)
     }, [search])
-    
-    
-    if(loading) return <LoadingDots />
-    
-    const listKecamatan = resultData?.list_kecamatan.map(d => {
-        return <option key={d.kode_wilayah} value={d.k3}>{d.nama_kecamatan}</option>
-    })
-
-    const listDesa = resultData?.list_desa.map(d => {
-        if (d.k3 === search.kecamatan) return <option key={d.kode_wilayah} value={d.k4}>{d.nama_deskel}</option>
-    })
-
-    const pencarianChangeEvenet = (e: any) => {
-        setSearch(p => ({ ...p, text: e.target.value }))
-    }
-
-    const kecamatanChangeEvent = (e: any) => {
-        setSearch(p => ({ ...p, kecamatan: e.target.value, desa: "" }))
-    }
-
-    const desaChangeEvent = (e: any) => {
-        setSearch(p => ({ ...p, desa: e.target.value }))
-    }
-
 
     return <div className="px-4 py-10">
         <PageTitle title="WISATA DESA" last_updated={resultData?.last_updated} />
@@ -62,18 +45,18 @@ export default function Wisata() {
         <div className="flex mt-9 flex-col">
             <div className="flex gap-x-5 pt-2">
                 <div className="flex relative">
-                    <input onChange={pencarianChangeEvenet} type="text" placeholder="Cari Desa/Kelurahan..." className="focus:border-blue-400 focus:shadow border bg-white text-sm border-slate-300 rounded text-neutral-600 w-full outline-none pl-2 pr-10 py-1" />
+                    <input onChange={(e) => setSearch(p => ({ ...p, text: e.target.value }))} type="text" placeholder="Cari Desa/Kelurahan..." className="focus:border-blue-400 focus:shadow border bg-white text-sm border-slate-300 rounded text-neutral-600 w-full outline-none pl-2 pr-10 py-1" />
                     <BiSearch className="absolute right-2 top-1/2 -translate-y-1/2 size-6 fill-slate-600" />
                 </div>
 
-                <select onChange={kecamatanChangeEvent} className="focus:border-blue-400 focus:shadow border text-sm bg-white border-slate-300 rounded text-neutral-600 w-1/4 outline-none pl-2 pr-4 py-1">
+                <select onChange={(e) => setSearch(p => ({ ...p, kecamatan: e.target.value, desa: "" }))} className="focus:border-blue-400 focus:shadow border text-sm bg-white border-slate-300 rounded text-neutral-600 w-1/4 outline-none pl-2 pr-4 py-1">
                     <option value="">Semua Kecamatan</option>
-                    {listKecamatan}
+                    {resultData?.list_kecamatan.map(d => <option key={d.kode_wilayah} value={d.k3}>{d.nama_kecamatan}</option>)}
                 </select>
 
-                <select onChange={desaChangeEvent} className="focus:border-blue-400 mr-auto focus:shadow border text-sm bg-white border-slate-300 rounded text-neutral-600 w-1/4 outline-none pl-2 pr-4 py-1">
+                <select onChange={(e) => setSearch(p => ({ ...p, desa: e.target.value }))} className="focus:border-blue-400 mr-auto focus:shadow border text-sm bg-white border-slate-300 rounded text-neutral-600 w-1/4 outline-none pl-2 pr-4 py-1">
                     <option value="">Semua Desa</option>
-                    {listDesa}
+                    {resultData?.list_desa.map(d => { if (d.k3 === search.kecamatan) return <option key={d.kode_wilayah} value={d.k4}>{d.nama_deskel}</option> })}
                 </select>
 
                 <ExportReportButton url="export/wisata" />
@@ -82,19 +65,19 @@ export default function Wisata() {
 
         <div className="grid grid-cols-3 gap-4 mt-5">
             {dataTodisplay?.map(w => {
-                return <WisataCard  
+                return <WisataCard
                     key={w.kode_wilayah}
                     foto={w.foto}
                     judul={w.judul}
                     subjudul={w.subjudul}
-                    url={"https://"+ w.slug_desa + ".digitaldesa.id/wisata/" + w.slug}
+                    url={"https://" + w.slug_desa + ".digitaldesa.id/wisata/" + w.slug}
                 />
             })}
         </div>
     </div>
 }
 
-const WisataCard = ({ foto, judul, subjudul, url }: {foto: string, judul: string, subjudul: string, url: string }) => {
+const WisataCard = ({ foto, judul, subjudul, url }: { foto: string, judul: string, subjudul: string, url: string }) => {
     return <a href={url} className="card bg-base-100 shadow-sm overflow-hidden">
         <figure>
             <img
