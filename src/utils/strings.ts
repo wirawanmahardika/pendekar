68bd6a73752e77a73127e11be0fcde7410caf1db
Ito Rijal <ophyeq@gmail.com>
2025-05-23 15:58:28 +0800

    feat: added pendekar demo

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -2,6 +2,29 @@ import defaultUser from '../assets/profil/default.png'
 import { CDN_URL } from './api';
 
 export const STRINGS: any = {
+  digikab: {
+    title: "SiCesa",
+    desc: "Sistem Cepat Informasi Desa",
+    nama: "SiCesa",
+    kabkota: "Kabupaten Ketapang",
+    slogan: "",
+    logo_kab_url: CDN_URL + "uploads/landing/logo-desa/ketapangkab.png",
+    logo_dss_url: '/img/logo/sicesa.png',
+    logo_background_image_login: 'ketapangkab.jpeg',
+    nama_user: "Alexander Wilyo, S.STP., M.Si.",
+    jabatan_user: "Bupati Ketapang",
+    nama_user_2: "Jamhuri Amir, S.H.",
+    logo_user: defaultUser,
+    logo_user_2: "/img/wakil.png",
+    menu_pelayanan: "hide",
+    menu_pengaduan: "hide",
+    theme: {
+      color_deep: "#0077B2",
+      color_normal: "#8ec5ff",
+      color_light: "#cddff7",
+      text_color: "#ffffff",
+    }
+  },
   ketapangkab: {
     title: "SiCesa",
     desc: "Sistem Cepat Informasi Desa",
