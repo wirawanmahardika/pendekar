18b1d67077e3922c7c532ebd7757ad06d1254425
richardenrico <richardenrico02@gmail.com>
2025-07-28 17:13:35 +0800

    chore: add react-date-range package

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 import App from './App.tsx'
 import './style.css'
 
